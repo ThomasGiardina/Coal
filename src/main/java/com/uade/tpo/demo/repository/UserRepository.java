8fa6a71037e4561812a8937a89ec3fd8465b0f0d
MarcoAmbrosini22 <mambrosini@uade.edu.ar>
2024-08-27 17:16:00 -0300

    Log/Register (casi juegos)

---
+++ src/main/java/com/uade/tpo/demo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.uade.tpo.demo.entity.Usuario;
+
+@Repository
+public interface UserRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String mail);
+}
